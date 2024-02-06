5e57e5775a3ddcdc7957af785ba84d1ef88e9419
Sergy <sergy_lopez_21@hotmail.com>
2024-02-06 23:46:30 +0100

    strlen

--- libft.h
+++ libft.h
@@ -13,6 +13,7 @@
 #ifndef LIBFT_H
 # define LIBFT_H
 # include "unistd.h"
+# include <stdio.h>
 
 int	ft_isalpha(int c);
 
@@ -23,5 +24,7 @@ int	ft_isalnum(int c);
 int	ft_isascii(int c);
 
 int	ft_isprint(int c);
+
+size_t ft_strlen(const char* str);
 
 #endif
