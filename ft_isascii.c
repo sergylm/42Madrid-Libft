13705377655f9e1b8bff2a9915ee1f6f037028d1
Sergy <sergy_lopez_21@hotmail.com>
2024-02-06 23:16:36 +0100

    isascii

--- ft_isascii.c
+++ ft_isascii.c
@@ -1,25 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_isascii.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: selopez- <selopez-@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/01/13 19:07:52 by selopez-          #+#    #+#             */
-/*   Updated: 2024/01/13 19:59:50 by selopez-         ###   ########.fr       */
+/*   Created: 2024/01/13 20:21:32 by selopez-          #+#    #+#             */
+/*   Updated: 2024/01/13 20:22:28 by selopez-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
-# include "unistd.h"
 
-int	ft_isalpha(int c);
-
-int	ft_isdigit(int c);
-
-int	ft_isalnum(int c);
-
-int	ft_isascii(int c);
-
-#endif
+int	ft_isascii(int c)
+{
+	if (c >= 0 && c <= 127 )
+		return (1);
+	else
+		return (0);
+}
