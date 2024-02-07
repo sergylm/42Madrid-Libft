0867ed1a0ff6990c0371c8042be77af7888f2abd
Sergy <sergy_lopez_21@hotmail.com>
2024-02-07 11:55:37 +0100

    ft_memcpy

---
+++ ft_memcpy.c
@@ -0,0 +1,15 @@
+# include "libft.h"
+
+void *ft_memcpy(void *dst, const void *src, size_t n)
+{
+    unsigned char *aux_dst;
+    unsigned char *aux_src;
+
+    if (src == NULL && dst == NULL)
+        return (NULL);
+    aux_dst = (unsigned char *) dst;
+    aux_src = (unsigned char *) src;
+    while (n--)
+        *aux_dst++ = *aux_src++;
+    return (dst);
+}
--- libft.h
+++ libft.h
@@ -31,4 +31,6 @@ void *ft_memset(void *str, int c, size_t n);
 
 void ft_bzero(void *str, size_t n);
 
+void *ft_memcpy(void *dst, const void *src, size_t n);
+
 #endif
