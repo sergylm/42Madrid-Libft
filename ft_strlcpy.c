9997d36c58de64ab16b6da639e3156101b25962d
Sergy <sergy_lopez_21@hotmail.com>
2024-02-07 12:05:13 +0100

    ft_strlcpy

---
+++ ft_strlcpy.c
@@ -0,0 +1,17 @@
+# include "libft.h"
+
+size_t	ft_strlcpy(char *dst, const char *src, size_t n)
+{
+	unsigned int	i;
+	unsigned int	len;
+
+	i = 0;
+	len = ft_strlen(src);
+	while (--n && src[i])
+	{
+		dst[i] = src[i];
+		i++;
+	}
+	dst[i] = '\0';
+	return (len);
+}
--- libft.h
+++ libft.h
@@ -35,4 +35,6 @@ void *ft_memcpy(void *dst, const void *src, size_t n);
 
 void *ft_memmove(void *dst, const void *src, size_t n);
 
+size_t	ft_strlcpy(char *dst, const char *src, size_t n);
+
 #endif
