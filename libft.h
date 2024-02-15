de87009193e12b019f5e80ae1ba20aa30de6dcbf
Sergy <sergy_lopez_21@hotmail.com>
2024-02-15 22:23:56 +0100

    ft_putendl_fd

--- libft.h
+++ libft.h
@@ -67,4 +67,6 @@ void    ft_striteri(char *s, void (*f)(unsigned int, char *));
 void    ft_putchar_fd(char c, int fd);
 
 void    ft_putstr_fd(char *s, int fd);
+
+void    ft_putendl_fd(char *s, int fd);
 #endif
