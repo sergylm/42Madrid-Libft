6db8c4d1f78494e7631354354e05de8d7088f65d
Sergy <sergy_lopez_21@hotmail.com>
2024-02-16 00:27:55 +0100

    ft_lstadd_back

---
+++ ft_lstadd_back.c
@@ -0,0 +1,16 @@
+#include "libft.h"
+
+void	ft_lstadd_back(t_list **lst, t_list *new)
+{
+	t_list	*back;
+
+	if(!new)
+		return ;
+	else if(!*lst)
+		*lst = new;
+	else
+	{
+		back = ft_lstlast(*lst);
+		back->next = new;
+	}
+}
