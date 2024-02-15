cdf0758e820091e183d55c196c151e74b8117018
Sergy <sergy_lopez_21@hotmail.com>
2024-02-15 23:53:51 +0100

    ft_lstsize

---
+++ ft_lstsize.c
@@ -0,0 +1,18 @@
+#include "libft.h"
+
+int	ft_lstsize(t_list *lst)
+{
+	t_list	*aux;
+	int		len;
+
+	if (!lst)
+		return (0);
+	aux = lst;
+	len = 1;
+	while(aux->next)
+	{
+		aux = aux->next;
+		len++;
+	}
+	return (len);
+}
