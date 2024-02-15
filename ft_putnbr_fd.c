dce0a32974a1d9387b7f6e049b349ee741f28915
Sergy <sergy_lopez_21@hotmail.com>
2024-02-15 23:03:16 +0100

    ft_putnbr_fd

---
+++ ft_putnbr_fd.c
@@ -0,0 +1,29 @@
+#include "libft.h"
+
+void	ft_putnbr_fd(int n, int fd)
+{
+	char	c;
+
+	if (n == -2147483648)
+	{
+		write(fd, "-2147483648", 11);
+	}
+	else
+	{
+		if (n < 0)
+		{
+			write(fd, "-", 1);
+			n *= -1;
+		}
+		if (n < 10)
+		{
+			c = (char) n + '0';
+			write(fd, &c, 1);
+		}
+		else
+		{
+			ft_putnbr_fd(n / 10, fd);
+			ft_putnbr_fd(n % 10, fd);
+		}
+	}
+}
