72377213addcad12a31aac762a1310d6c83d6a79
Sergy <sergy_lopez_21@hotmail.com>
2024-02-16 10:33:49 +0100

    ft_lstdelone

--- libft.h
+++ libft.h
@@ -87,4 +87,6 @@ int     ft_lstsize(t_list *lst);
 t_list  *ft_lstlast(t_list *lst);
 
 void    ft_lstadd_back(t_list **lst, t_list *new);
+
+void	ft_lstdelone(t_list *lst, void (*del)(void *));
 #endif
