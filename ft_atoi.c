bc485b10e6fb8c9ed540be315d89d8470d37471a
Sergy <sergy_lopez_21@hotmail.com>
2024-02-18 00:13:26 +0100

    bug fixed paco

--- ft_atoi.c
+++ ft_atoi.c
@@ -1,23 +1,25 @@
-# include "libft.h"
+#include "libft.h"
 
-int ft_atoi(const char *str)
+int	ft_atoi(const char *str)
 {
-    int i;
-    int sign;
+	int	i;
+	int	sign;
 
-    i = 0;
-    sign = 1;
-    if (*str == '-' || *str == '+')
-    {
-        if (*str++ == '-')
-            sign = -1;
-    }
-    
-    while(*str)
-    {
-        if (!ft_isdigit(*str))
-            return (0);
-        i = (i * 10) + (*str++ - 48);
-    }
-    return (i * sign);
+	i = 0;
+	sign = 1;
+	while ((*str == ' ') || (*str == '\t') || (*str == '\v') || (*str == '\n')
+		|| (*str == '\f') || (*str == '\r'))
+		str++;
+	if (*str == '-' || *str == '+')
+	{
+		if (*str++ == '-')
+			sign = -1;
+	}
+	while (*str)
+	{
+		if (!ft_isdigit(*str))
+			return (i * sign);
+		i = (i * 10) + (*str++ - 48);
+	}
+	return (i * sign);
 }
