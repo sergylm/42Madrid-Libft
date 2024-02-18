7f0ce227c316fbcf8f93af4697b83cc5f611016d
Sergy <sergy_lopez_21@hotmail.com>
2024-02-18 14:02:30 +0100

    update

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,15 +6,13 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 	t_list	*new;
 
 	first = NULL;
-	if (!f || !del)
-		return (first);
 	while (lst)
 	{
 		new = ft_lstnew((*f)(lst->content));
 		if (!new)
 		{
 			ft_lstclear(&first, del);
-			return (first);
+			return (NULL);
 		}
 		ft_lstadd_back(&first, new);
 		lst = lst->next;
