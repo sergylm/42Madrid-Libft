6c6490c177564c08061eaec38b8b7d1df99a2767
Sergy <sergy_lopez_21@hotmail.com>
2024-02-18 02:24:08 +0100

    paco + norminette

--- ft_substr.c
+++ ft_substr.c
@@ -3,7 +3,11 @@
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*result;
-	
+
+	if (start > ft_strlen(s) || !s)
+		return (ft_strdup(""));
+	if (len > ft_strlen(s) - start)
+		len = ft_strlen(s) - start;
 	result = malloc(len + 1);
 	if (result)
 		ft_strlcpy(result, &s[start], len + 1);
